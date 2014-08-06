5e7b314b49b9b9867824d2829578ad2bf8f03065
Joseph Hardin <josephhardinee@gmail.com>
2014-08-05 18:55:45 -0600

    Added 2DVD support for MC3E

--- pydisdrometer/__init__.py
+++ pydisdrometer/__init__.py
@@ -3,3 +3,4 @@ from .ParsivelNasaGVReader import read_parsivel_nasa_gv
 from .JWDReader import read_jwd
 from .aux_readers.GPMApuWallopsRawReader import read_gpm_nasa_apu_raw_wallops
 from .aux_readers.NASA_2DVD_reader import read_2dvd_sav_nasa_gv
+from .aux_readers.NASA_2DVD_reader import read_2dvd_dsd_nasa_gv
--- pydisdrometer/aux_readers/NASA_2DVD_reader.py
+++ pydisdrometer/aux_readers/NASA_2DVD_reader.py
@@ -2,7 +2,6 @@
 import numpy as np
 from ..DropSizeDistribution import DropSizeDistribution
 
-import csv
 import scipy.io
 
 
@@ -14,7 +13,7 @@ def read_2dvd_sav_nasa_gv(filename, campaign='ifloods'):
     This reader processes the .sav files generated.
 
     Usage:
-    dsd = read_2dvd_sav_nasa_gv(filename, campaign='MC3E_dsd')
+    dsd = read_2dvd_sav_nasa_gv(filename, campaign='ifloods')
 
     Current Options for campaign are:
 
@@ -25,12 +24,14 @@ def read_2dvd_sav_nasa_gv(filename, campaign='ifloods'):
 
     '''
 
-    reader = NASA_2DVD_reader(filename, campaign)
+    reader = NASA_2DVD_sav_reader(filename, campaign)
 
     if reader:
         dsd = DropSizeDistribution(reader.time, reader.Nd, reader.spread,
-                                   velocity=reader.velocity, diameter=reader.diameter,
-                                   bin_edges=reader.bin_edges, rain_rate=reader.rain_rate)
+                                   velocity=reader.velocity,
+                                   diameter=reader.diameter,
+                                   bin_edges=reader.bin_edges,
+                                   rain_rate=reader.rain_rate)
         return dsd
 
     else:
@@ -39,10 +40,46 @@ def read_2dvd_sav_nasa_gv(filename, campaign='ifloods'):
     del(reader)
 
 
-class NASA_2DVD_reader(object):
+def read_2dvd_dsd_nasa_gv(filename, campaign='mc3e'):
+    '''
+    Takes a filename pointing to a 2D-Video Disdrometer NASA Field Campaign
+     _dsd file and returns a drop size distribution object.
+
+    This reader processes the _dsd files generated.
+
+    Usage:
+    dsd = read_2dvd_dsd_nasa_gv(filename, campaign='mc3e')
+
+    Current Options for campaign are:
+
+    'mc3e'
+
+    Returns:
+    DropSizeDistrometer object
 
     '''
-    This class reads and parses 2dvd disdrometer data from nasa ground campaigns. 
+
+    reader = NASA_2DVD_dsd_reader(filename, campaign)
+
+    if reader:
+        dsd = DropSizeDistribution(reader.time, reader.Nd, reader.spread,
+                                   velocity=reader.velocity,
+                                   diameter=reader.diameter,
+                                   bin_edges=reader.bin_edges)
+        return dsd
+
+    else:
+        return None
+
+    del(reader)
+
+
+class NASA_2DVD_sav_reader(object):
+
+    '''
+    This class reads and parses 2dvd disdrometer data from nasa ground
+    campaigns.
+
     Use the read_2dvd_sav_nasa_gv() function to interface with this.
     '''
 
@@ -84,6 +121,46 @@ class NASA_2DVD_reader(object):
         minute = np.array([float(mn) for mn in record.minute[0]])
         return hour + minute
 
-
-
     supported_campaigns = ['ifloods']
+
+
+class NASA_2DVD_dsd_reader(object):
+
+    '''
+    This class reads and parses 2dvd disdrometer data from NASA ground
+    campaigns.
+
+    Use the read_2dvd_dsd_nasa_gv() function to interface with this.
+    '''
+
+    def __init__(self, filename, campaign):
+        '''
+        Handles setting up a NASA 2DVD Reader  Reader
+        '''
+        MIN_IN_DAY = 1440
+        self.time = np.arange(MIN_IN_DAY)  # Time in minutes
+        self.Nd = np.zeros((MIN_IN_DAY, 50))
+        self.notes = []
+
+        if not campaign in self.supported_campaigns:
+            print('Campaign not supported')
+            return
+
+        with open(filename) as input:
+            for line in input:
+                data_array = line.split()
+                time_min = int(data_array[2])*60 + int(data_array[3])
+                self.Nd[time_min, :] = [float(value) for value in data_array[4:]]
+
+        self.diameter = np.arange(0.1, 10.1, .2)
+        self.velocity =[0.248, 1.144, 2.018, 2.858, 3.649, 4.349, 4.916, 5.424, 5.892, 6.324,
+                        6.721, 7.084, 7.411, 7.703, 7.961, 8.187, 8.382, 8.548, 8.688, 8.805,
+                        8.900, 8.977, 9.038, 9.084, 9.118, 9.143, 9.159, 9.169, 9.174, 9.175,
+                        9.385, 9.415, 9.442, 9.465, 9.486, 9.505, 9.521, 9.536, 9.549, 9.560,
+                        9.570, 9.570, 9.570, 9.570, 9.570, 9.570, 9.570, 9.570, 9.570, 9.570]
+
+        self.bin_edges = np.array(range(0, 51)) * 0.2
+
+        self.spread = np.array([0.2]*50)
+
+    supported_campaigns = ['mc3e']
