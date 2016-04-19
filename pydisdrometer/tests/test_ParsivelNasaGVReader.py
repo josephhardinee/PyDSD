34904bf8292fd3bf11059299a4aceea26938b88c
Joseph C. Hardin <josephhardinee@gmail.com>
2016-04-19 10:19:19 -0700

    Added testfiles for nasa apu and dvd readers. Added some unit tests for the nasa parsivel reader

---
+++ pydisdrometer/tests/test_ParsivelNasaGVReader.py
@@ -0,0 +1,28 @@
+import numpy as np
+from unittest import TestCase
+
+from .. import read_parsivel_nasa_gv
+import datetime
+
+import os
+
+class testParsivelNASAGVReader(TestCase):
+
+    def test_read_file(self):
+        ''' Test whether this function can read in the apu_nasa_mc3e_dsd.txt file in testdata.
+        '''
+
+        dsd = read_parsivel_nasa_gv('testdata/apu_nasa_mc3e_dsd.txt')
+        self.assertIsNotNone(dsd, 'reader returned a None object')
+
+
+    def test_reader_reads_in_time(self):
+        ''' Make sure our test file has correct time. Test a few aspects of time. '''
+        time_entries=3
+        first_time_entry = datetime.datetime(2011,5,20,8,28)
+
+        dsd = read_parsivel_nasa_gv('testdata/apu_nasa_mc3e_dsd.txt')
+        self.assertEquals(time_entries, len(dsd.time['data']), "Incorrect number of time entries read in.")
+
+        self.assertEquals(first_time_entry, dsd.time['data'][0], "Initial time was incorrect")
+
