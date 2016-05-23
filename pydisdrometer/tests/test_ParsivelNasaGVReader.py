c3b2092ec7667da8ec761ae0678bcb210e77edb1
Joseph C. Hardin <josephhardinee@gmail.com>
2016-05-23 06:31:38 -0700

    Adding tests for ParsivelNasaGVReader

--- pydisdrometer/tests/test_ParsivelNasaGVReader.py
+++ pydisdrometer/tests/test_ParsivelNasaGVReader.py
@@ -6,13 +6,11 @@ import datetime
 
 import os
 
-class testParsivelNASAGVReader(TestCase):
 
+class testParsivelNASAGVReader(TestCase):
     def test_read_file(self):
         ''' Test whether this function can read in the apu_nasa_mc3e_dsd.txt file in testdata.
         '''
 
         dsd = read_parsivel_nasa_gv('testdata/apu_nasa_mc3e_dsd.txt')
         self.assertIsNotNone(dsd, 'reader returned a None object')
-
-
