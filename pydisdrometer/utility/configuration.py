ec4631b4022156a6c9141d96c777a6dde4d00cba
Joseph C. Hardin <josephhardinee@gmail.com>
2016-09-22 14:50:09 -0700

    Added a configuration parser, and converted dsd moments and radar moments to use the new meta data fields

---
+++ pydisdrometer/utility/configuration.py
@@ -0,0 +1,32 @@
+import json
+from copy import copy
+import os
+
+
+
+class Configuration(object):
+    ''' Class to store PyDisdrometer configuration options
+
+    Attributes:
+    -----------
+
+    '''
+    config_dir = os.path.dirname(os.path.abspath(__file__))
+    metadata_config_file = os.path.join(config_dir, 'metadata.json')
+
+    def __init__(self):
+        self.metadata = self.load_metadata_config()
+
+    def load_metadata_config(self):
+        ''' Load the metadata configuration file and return the dictionary'''
+        return json.load(open(self.metadata_config_file))
+
+
+    def fill_in_metadata(self, field, data):
+        metadata = self.metadata[field].copy()
+        metadata['data'] = copy(data)
+        return metadata
+
+
+
+
