beb6a36e347f43b874bcec0dd5ae276a40909aef
Joseph C. Hardin <josephhardinee@gmail.com>
2017-02-14 11:06:21 -0800

    Cleaned up tests for parsivelnasagvreader to get rid of an extraneous print statement.

--- pydisdrometer/tests/test_ParsivelNasaGVReader.py
+++ pydisdrometer/tests/test_ParsivelNasaGVReader.py
@@ -13,14 +13,13 @@ class TestParsivelNASAGVReader(TestCase):
     def setUp(self):
         self.dsd = read_parsivel_nasa_gv('testdata/apu_nasa_mc3e_dsd.txt')
 
-    def test_read_file(self):
+    def test_read_file_does_not_return_none(self):
         ''' Test file apu_nasa_mc3e_dsd.txt reads in and returns non None object.
         '''
-        print(type(self.dsd))
-
         self.assertIsNotNone(self.dsd, 'reader returned a None object')
 
     def test_number_entries(self):
         """ Number of entries in apu_nasa_mc3e_dsd.txt file is 5 and consistent.
         """
         self.assertTrue(len(self.dsd.Nd['data'])) == 5
+
