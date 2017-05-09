d51f29bfe89f76f19dc3716fa4af8bacf20fa8b2
Joseph C. Hardin <joseph.hardin@pnnl.gov>
2017-05-08 23:46:46 -0700

    Trying doctr

--- continuous_integration/build_docs.sh
+++ continuous_integration/build_docs.sh
@@ -1,33 +1,4 @@
-#!/bin/bash
-# Adapted from the ci/build_docs.sh file from the pandas project
-# https://github.com/pydata/pandas
-set -e
-
 cd "$TRAVIS_BUILD_DIR"
-
-echo "Building Docs"
-conda install -q sphinx
-
-mv "$TRAVIS_BUILD_DIR"/docs /tmp
-cd /tmp/docs
+cd docs
 make html
-
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ $TRAVIS_SECURE_ENV_VARS == 'true' ]; then
-    cd /tmp/docs/build/html
-    git config --global user.email "pydisdrometer-docs-bot@example.com"
-    git config --global user.name "pydisdrometer-docs-bot"
-
-    git init
-    touch README
-    git add README
-    git commit -m "Initial commit" --allow-empty
-    git branch gh-pages
-    git checkout gh-pages
-    touch .nojekyll
-    git add --all .
-    git commit -m "Version" --allow-empty -q
-    git remote add origin https://$GH_TOKEN@github.com/https://github.com/josephhardinee/PyDisdrometer.git &> /dev/null
-    git push origin gh-pages -fq &> /dev/null
-fi
-
-exit 0
+doctr deploy .
