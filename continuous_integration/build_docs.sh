f5b8f6ad5898a745df7b5c7ad8dbdec7a161145f
Joseph C. Hardin <joseph.hardin@pnnl.gov>
2017-05-09 00:37:32 -0700

    Giving doctr another try

--- continuous_integration/build_docs.sh
+++ continuous_integration/build_docs.sh
@@ -1,35 +1,7 @@
-#!/bin/bash
-# Adapted from the ci/build_docs.sh file from the pandas project
-# https://github.com/pydata/pandas
 set -e
-
-cd "$TRAVIS_BUILD_DIR"
-
-echo "Building Docs"
-conda install --yes sphinx pillow=3.3.1
-
-cd "$TRAVIS_BUILD_DIR"/docs
+pip install doctr
+doctr deploy
+cd docs
 make html
-mv "$TRAVIS_BUILD_DIR"/docs/build/html /tmp
-
-
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ $TRAVIS_SECURE_ENV_VARS == 'true' ]; then
-
-    cd "$TRAVIS_BUILD_DIR"
-    rm -rf *
-    git init .
-    git checkout gh-pages
-
-    mv /tmp/html/* ./
-
-    git config --global user.email "pydisdrometer-docs-bot@example.com"
-    git config --global user.name "pydisdrometer-docs-bot"
-
-    touch .nojekyll
-    git add --all .
-    git commit -m "Version" --allow-empty -q
-    git remote add origin https://$GH_TOKEN@github.com/https://github.com/josephhardinee/PyDisdrometer.git &> /dev/null
-    git push origin gh-pages -fq &> /dev/null
-fi
-
-exit 0
+cd ..
+doctr deploy .
