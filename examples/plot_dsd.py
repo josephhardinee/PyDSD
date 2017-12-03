8c51b0154c1b0a565e2ebd2d0d0d5ab0a22e0c84
Joseph C. Hardin <joseph.hardin@pnnl.gov>
2017-12-03 11:34:30 -0800

    Fixed one of the examples that had a fixed path

--- examples/plot_dsd.py
+++ examples/plot_dsd.py
@@ -12,8 +12,6 @@ import matplotlib.pyplot as plt
 import pydsd as pyd
 
 filename = '../testdata/sgpdisdrometerC1.b1.20110427.000000_test_jwd_b1.cdf'
-filename = '/Users/hard505/Dropbox/research/parsivel_apu14_mc3e_N363558.62_W0972534.21_20110425_dsd.txt'
-# dsd = pyd.aux_readers.ARM_JWD_Reader.read_arm_jwd_b1(filename)
 dsd = pyd.read_parsivel_nasa_gv(filename)
 #Read in the Parsivel File
 dsd.calculate_dsd_parameterization()
