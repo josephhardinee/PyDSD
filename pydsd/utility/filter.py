32f116ad93f4ecd16fb9837b563ffeb26cd53d34
Joseph Hardin <josephhardinee@gmail.com>
2020-01-28 14:25:46 -0800

    Better drop spectrum support (#92)
    This adds better support for drop spectrums. Most work is focused on Parsivels to start with, but the support is fairly general. This adds a series of new features:
    readers that set the field drop_spectrum, and attribute spectrum_fall_velocity will have this tracked. For an example see l105-110 of the ARM_APU_reader.py
    
    This also adds support on the DSD object for converting spectra into N(D).
    
    Also added are several filtering/QC operations. Chief among these is the filter_spectrum_with_parsivel_matrix function in utility.filter. This filters the spectral matrix (for parsivels) with arbitrary fall speed assumptions. Similarly there is a filter_nd_on_dropsize added. Several other supporting features have been added. I need to make a notebook demonstrating these at some point.
    
    Individual updates:
    
    * Renamed notebook to reflect change to PyDSD
    
    * Initial support for reading in drop spectrum and storing them in fields
    
    * Added support for PCM 50% fall speed filtering
    
    * Added parsivel spectrum filtering, recovery of Nd from spectrum, and filtering on nd dropsize
    
    * Cleaned up parsivel spectrum filtering, added replace option, track history
    
    * Cleaned up several instances of inplace to be replace to make the purpose more clear
    
    * Run through black and removed default parsivel matrix
    
    * Removed Python 3.5 as we use f-strings heavily now
    
    * Moved py3.8 to allow failures for now. Issues with pyqt and sip that I'll troubleshoot later

---
+++ pydsd/utility/filter.py
@@ -0,0 +1,150 @@
+import numpy as np
+import copy
+
+
+def filter_spectrum_with_parsivel_matrix(
+    dsd, over_fall_speed=None, under_fall_speed=None, replace=True
+):
+    """ Filter a drop spectrum using fall speed matrix for Parsivels.  This requires that velocity is set on the object
+    for both raw spectra and calculated terminal fall speed. If terminal fall speed is not available, this can be calculated
+    using pydsd.
+    Parameters
+    ----------
+    over_fall_speed: float
+        Filter out drops more than this factor of terminal fall speed.
+    under_fall_speed: float
+        Filter out drops more than this factor under terminal fall speed.
+
+
+    Returns
+    -------
+    filtered_spectrum_data_array: np.ndarra
+        Filtered Drop Spectrum Array
+
+    Example
+    -------
+    filter_spectrum_with_parsivel_matrix(dsd, over_fall_speed=.5, under_fall_speed=.5, replace=True)
+    """
+    # TODO: This can be easily generalized for other disdrometers.
+    terminal_fall_speed = dsd.velocity["data"]
+    spectra_velocity = dsd.spectrum_fall_velocity["data"]
+
+    pcm_matrix = np.zeros((32, 32))
+    for idx in np.arange(0, 32):
+        pcm_matrix[idx] = np.logical_and(
+            spectra_velocity > (terminal_fall_speed[idx] * (1 - under_fall_speed)),
+            spectra_velocity < (terminal_fall_speed[idx] * (1 + over_fall_speed)),
+        )
+
+    pcm_matrix = pcm_matrix.T.astype(int)
+
+    if replace:
+        dsd.fields["drop_spectrum"]["data"] = dsd.fields["drop_spectrum"][
+            "data"
+        ] * pcm_matrix
+        dsd.fields["drop_spectrum"]["history"] = dsd.fields["drop_spectrum"].get(
+            "history", ""
+        ) + "Filtered for speeds above {over_fall_speed} of Vt and below {under_fall_speed} of Vt"
+    else:
+        return dsd.fields["drop_spectrum"]["data"] * pcm_matrix
+
+
+def filter_nd_on_dropsize(dsd, drop_min=None, drop_max=None, replace=True):
+    """ Filter Nd field based on a min and/or max dropsize.
+    
+    Parameters
+    ----------
+    dsd: `DropSizeDistribution` object
+        DSD object to base filtering on
+    drop_min: float
+        Filter drops under drop_min (mm) in size.
+    drop_max: float
+        Filter drops larger than drop_max (mm) in size.
+    replace: boolean
+        Whether to overwrite the Nd in fields. If replacing, no value is returned.
+
+    Returns
+    -------
+    Nd: dictionary
+        Updated Nd dictionary. Data and a history field.
+    """
+    diameter = dsd.diameter["data"]
+
+    if drop_min is None:
+        drop_min = 0
+    if drop_max is None:
+        drop_max = diameter[-1] + 100
+
+    mask = np.logical_and(diameter > drop_min, diameter < drop_max)
+
+    if replace:
+        dsd.Nd["data"] = dsd.Nd["data"] * mask
+        dsd.Nd["history"] = dsd.Nd.get(
+            "history", ""
+        ) + f"\nFiltered between {drop_min} and {drop_max}"
+    else:
+        Nd = copy.deepcopy(dsd.Nd)
+        Nd["data"] = Nd["data"] * mask
+        Nd["history"] = dsd.Nd.get(
+            "history", ""
+        ) + f"Filtered between {drop_min} and {drop_max}\n"
+        return Nd
+
+
+def filter_spectrum_on_dropsize(dsd, drop_min=None, drop_max=None, replace=True):
+    """ Filter Nd field based on a min and/or max dropsize.
+    
+    Parameters
+    ----------
+    dsd: `DropSizeDistribution` object
+        DSD object to base filtering on
+    drop_min: float
+        Filter drops under drop_min (mm) in size.
+    drop_max: float
+        Filter drops larger than drop_max (mm) in size.
+    replace: boolean
+        Whether to replace the spectrum stored on dsd object. If set, no data is returned.
+
+    Returns
+    -------
+    Nd: dictionary
+        Updated Nd dictionary. Data and a history field.
+    """
+    diameter = dsd.diameter["data"]
+
+    if drop_min is None:
+        drop_min = 0
+    if drop_max is None:
+        drop_max = diameter[-1] + 100
+
+    mask = np.logical_and(diameter > drop_min, diameter < drop_max)
+
+    if replace:
+        dsd.Nd["data"] = dsd.Nd["data"] * mask
+        dsd.Nd["history"] = dsd.Nd.get(
+            "history", ""
+        ) + f"\nFiltered between {drop_min} and {drop_max}"
+    else:
+        Nd = copy.deepcopy(dsd.Nd)
+        Nd["data"] = Nd["data"] * mask
+        Nd["history"] = dsd.Nd.get(
+            "history", ""
+        ) + f"Filtered between {drop_min} and {drop_max}\n"
+        return Nd
+
+
+def parsivel_sampling_area(diameter):
+    """ Calculate effective sampling area for Parsivels
+    
+    Parameters
+    ----------
+    diameter: np.ndarray or float
+        Drop Diameter
+    
+    Returns
+    -------
+    Aeff: np.ndarray or float
+        Effective Sampling Area
+    """
+    return 180 * (30 - 0.5 * diameter)
+
