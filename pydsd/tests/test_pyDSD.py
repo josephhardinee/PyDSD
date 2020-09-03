9e375531dbe09b324f5b3c5b9e6804a1a9332032
Joseph C. Hardin <josephhardinee@gmail.com>
2020-09-03 09:03:21 -0700

    Added test data for arm ld tests

---
+++ pydsd/tests/test_pyDSD.py
@@ -0,0 +1,9 @@
+import pydsd
+from unittest import TestCase
+
+
+class TestPyDSDOverall(TestCase):
+
+    def test_import(self):
+        """ PyDSDOverall: Test whether the PyDSD can be imported properly"""
+        import pydsd
