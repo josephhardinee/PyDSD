Bugfix regresion spectrum calculation effective area arguments (#99)
* Added some fixes for passing in effective sampling area as a function not being handled appropriately. Also added regressions tests to make sure this doesn't become issue in future

* Added test data for arm ld tests

* Ran test_DropSizeDistribution.py through black for formatting

pydsd/tests/test_DropSizeDistribution.py
@@ -4,8 +4,19 @@ import numpy as np
 import copy
 
 from ..aux_readers import ARM_Vdis_Reader
+from ..io import ARM_vdisdrops_reader
 from ..io.NetCDFWriter import write_netcdf
 from .. import DropSizeDistribution
+from ..aux_readers import ARM_APU_reader
+from ..utility import filter
+
+
+@pytest.fixture
+def two_dvddrops_open_test_file(tmpdir):
+    filename_in = "testdata/corvdisdropsM1.b1.20181214.020816.cdf"
+    filename_out = tmpdir + "test_vdisdrops.nc"
+    dsd = ARM_vdisdrops_reader.read_arm_vdisdrops_netcdf(filename_in)
+    return dsd
 
 
 @pytest.fixture
@@ -16,7 +27,15 @@ def two_dvd_open_test_file(tmpdir):
     return dsd
 
 
-class TestNetCDFWriter(object):
+@pytest.fixture
+def parsivel_open_test_file(tmpdir):
+    filename_in = "testdata/corldM1.b1.20181214.000000.cdf"
+    filename_out = tmpdir + "test_pars.nc"
+    dsd = ARM_APU_reader.read_parsivel_arm_netcdf(filename_in)
+    return dsd
+
+
+class TestDropSizeDistribution(object):
     """
     Test module for the DropSizeDistribution"
     """
@@ -49,10 +68,9 @@ class TestNetCDFWriter(object):
         two_dvd_open_test_file.fields["Nd"]["data"][0, 1] = 2
         two_dvd_open_test_file.calculate_dsd_parameterization()
         assert np.isclose(
-            two_dvd_open_test_file.fields["D0"]["data"][0], .198, rtol=.01
+            two_dvd_open_test_file.fields["D0"]["data"][0], 0.198, rtol=0.01
         )  # One percent tolerance is closer than my hand calcs.
 
-    @pytest.mark.dependency()
     def test_saving_of_scatter_table(self, two_dvd_open_test_file, tmpdir):
         two_dvd_open_test_file.calculate_radar_parameters()
         two_dvd_open_test_file.save_scattering_table(tmpdir + "/test_scatter.scatter")
@@ -65,4 +83,33 @@ class TestNetCDFWriter(object):
         dsd_2.save_scattering_table(tmpdir + "/test_scatter.scatter")
         two_dvd_open_test_file.calculate_radar_parameters(
             scatter_table_filename=tmpdir + "/test_scatter.scatter"
+        )
+
+    def test_calculate_spectrum_accepts_function(self, parsivel_open_test_file):
+        parsivel_open_test_file.calculate_dsd_from_spectrum(
+            effective_sampling_area=filter.parsivel_sampling_area
+        )
+        assert (
+            parsivel_open_test_file.fields["Nd"]["source"]
+            == "Calculated from spectrum."
+        )
+
+    def test_calculate_spectrum_no_optional_args_with_effective_area_on_object_set(
+        self, two_dvddrops_open_test_file
+    ):
+        two_dvddrops_open_test_file.calculate_dsd_from_spectrum()
+        assert (
+            two_dvddrops_open_test_file.fields["Nd"]["source"]
+            == "Calculated from spectrum."
+        )
+
+    def test_calculate_spectrum_accepts_array_arg(self, two_dvddrops_open_test_file):
+        two_dvddrops_open_test_file.calculate_dsd_from_spectrum(
+            effective_sampling_area=two_dvddrops_open_test_file.effective_sampling_area[
+                "data"
+            ]
+        )
+        assert (
+            two_dvddrops_open_test_file.fields["Nd"]["source"]
+            == "Calculated from spectrum."
         )